7b065cdaa74386b046554feecb6d7e07dbb988df
David Zhao <zhaomaster95@gmail.com>
2018-11-23 13:46:46 -0500

    Colored time series by user

--- bum.py
+++ bum.py
@@ -5,20 +5,17 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.offline as offline
 
+from functools import reduce
+
 import ipdb
 
 
 def parse_data(lines):
-    states = {}
     prev = ""
     ignore = ["-", "`", ";", "|"]
     clean_output = []
-    cutoffs = []
-    counter = 0
     for line in lines:
         command = line[:-1]
-        if command == "delimiter":
-            cutoffs.append(counter)
         if len(command) == 0:
             continue
 
@@ -34,26 +31,33 @@ def parse_data(lines):
 
         if prev != "":
             clean_output.append(prev)
-            if prev not in states:
-                states[prev] = 1
-            else:
-                states[prev] += 1
-            counter += 1
             prev = ""
-
+            
         prev += command
 
-    print(cutoffs)
-    return states, clean_output, cutoffs
+    return clean_output
 
 
-def create_transition_matrix(freq, index, num_states, filtered_commands):
+def parse_all_data(lines):
+    contents = list(map(lambda x: parse_data(x), lines)) 
+    states = {}
+    for file_content in contents:
+        for command in file_content:
+            if command in states:
+                states[command] += 1
+            else:
+                states[command] = 1
+    return contents, states
+
+
+def create_transition_matrix(index, num_states, filtered_commands):
+    commands = reduce(lambda x,y : x + y, filtered_commands, [])
     P = np.zeros(shape=(num_states, num_states))
-    prev = filtered_commands[0]
-    for command in filtered_commands[1:]:
+    prev = commands[0]
+    for command in commands[1:]:
         prev_idx = index[prev] - 1
         cur_idx = index[command] - 1
-        P[prev_idx,cur_idx] += 1.0
+        P[prev_idx, cur_idx] += 1.0
         prev = command
 
     for i in range(num_states):
@@ -71,83 +75,93 @@ def plot_state_distribution(freq):
     offline.plot([trace], image='png', filename = "emperical_distribution")
 
 
-def plot_time_series(xlen, filtered_commands, num_states, index, cutoffs):
+def plot_time_series(xlen, filtered_commands, num_states, index, freq):
     """
     ets is short for emperical time series
     """
-    if xlen <= cutoffs[0]:
-        x = [i+1 for i in range(xlen)]
+    colors = [
+            "0,0,0",
+            "0,0,255",
+            "0,255,0",
+            "0,255,255",
+            "255,0,0",
+            "255,0,255",
+            "255,255,0",
+            "200,200,200",
+            "125, 125, 125",
+            ]
+    prev = 0
+    i=0
+    data = []
+    layout = go.Layout(
+            yaxis=dict(
+                tickvals = [i+1 for i in range(num_states)],
+                ticktext = [i[0] for i in reversed(freq)]
+                ),
+            )
+
+    for file_commands in filtered_commands:
+        x = [i+1 for i in range(prev, prev + len(file_commands))]
         y = []
-        
-        for command in filtered_commands[:xlen]:
+        for command in file_commands:
             idx = index[command]
             y.append(idx)
-
-        trace = go.Scatter(x=x, y=y, mode='markers')
-        offline.plot(
-                [trace],
-                image='png',
-                filename = "ets_{}_states_{}_steps".format(num_states, xlen))
-    else:
-        colors = [
-                "0,0,0",
-                "0,0,255",
-                "0,255,0",
-                "0,255,255",
-                "255,0,0",
-                "255,0,255",
-                "255,255,0",
-                "255,255,255",
-                "125, 125, 125",
-                ]
-        i = 0
-        data = []
-        cutoffs.append(0)
-        while xlen > 0 and i < 9:
-            x = [i+1 for i in range(cutoffs[i-1], cutoffs[i])]
-            y = []
-            xlen -= cutoffs[i]
-            for command in filtered_commands[cutoffs[i-1]:cutoffs[i]+1]:
-                idx = index[command]
-                y.append(idx)
-            trace = go.Scatter(
-                    x=x,
-                    y=y,
-                    mode='markers',
-                    marker= dict(color='rgb({})'.format(colors[i])),
-                    )
-            data.append(trace)
-            i += 1
-            if i >= 9:
-                break
-        
-        ipdb.set_trace()
-        offline.plot(
-                data,
-                image='png',
-                filename = "ets_{}_states_{}_steps".format(num_states, xlen)
+        trace = go.Scatter(
+                x=x,
+                y=y,
+                mode='markers',
+                marker= dict(color='rgb({})'.format(colors[i])),
                 )
+        data.append(trace)
+        prev += len(file_commands)
+        i += 1
+
+    ipdb.set_trace() 
+    fig = go.Figure(data=data, layout=layout)
+    offline.plot(
+            fig,
+            image='png',
+            filename = "ets_{}_states_{}_steps".format(num_states, xlen)
+            )
+
 
 def main(args):
     num_states = int(args.states)
     xlen = int(args.timelength) 
-    f = open('delimitedcommands', 'r')
-    lines = f.readlines()
-    f.close()
+    input_files = [
+            "user0",
+            "user1",
+            "user2",
+            "user3",
+            "user4",
+            "user5",
+            "user6",
+            "user7",
+            "user8",
+            ]
+    all_unfiltered_content = []
+    for input_file in input_files:
+        f = open(input_file)
+        content = f.readlines()
+        f.close()
+        all_unfiltered_content.append(content)
 
-    states, clean_output, cutoffs = parse_data(lines)
-    freq = sorted([val for key, val in states.items()])
-    filtered_states = {}
-    for key, val in states.items():
+    cleaned_inputs, all_freq = parse_all_data(all_unfiltered_content)
+    freq = sorted([val for key, val in all_freq.items()])
+    filtered_freq = {}
+    for key, val in all_freq.items():
         if val in freq[-num_states:]:
-            filtered_states[key] = val
+            filtered_freq[key] = val
 
     filtered_commands = []
-    for line in clean_output:
-        if line in filtered_states:
-            filtered_commands.append(line)
+    for clean_file in cleaned_inputs:
+        out = []
+        for command in clean_file:
+            if command in filtered_freq:
+                out.append(command)
+        filtered_commands.append(out)
 
-    freq = [(key, val) for key, val in filtered_states.items()]
+    freq = [(key, val) for key, val in filtered_freq.items()]
     freq.sort(key=lambda tup: tup[1])
 
     index = {}
@@ -155,11 +169,11 @@ def main(args):
         key, val = t
         index[key] = num_states-i
 
-    P = create_transition_matrix(freq, index, num_states, filtered_commands)
+    P = create_transition_matrix(index, num_states, filtered_commands)
     np.savetxt("transition_top_{}.csv".format(num_states), P, delimiter=",")
 
     if args.ets:
-        plot_time_series(xlen, filtered_commands, num_states, index, cutoffs)
+        plot_time_series(xlen, filtered_commands, num_states, index, freq)
     if args.sd:
         plot_state_distribution(freq)
 
